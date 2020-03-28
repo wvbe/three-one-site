330e8bf6c4c4ff89fb6910c6447cc484dc241b43
wvbe <wybe@x-54.com>
2020-03-28 17:41:55 +0100

    Committing the built files manually

---
+++ precache-manifest.ae68deccee3e1e9a7a32efb0f8f45fbd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e634c928a250536536dd051e82c78528",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a4a279b0cc382137f89",
+    "url": "./static/js/2.4a96be7f.chunk.js"
+  },
+  {
+    "revision": "aa752c17a87949aca852",
+    "url": "./static/js/main.42c2052f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
